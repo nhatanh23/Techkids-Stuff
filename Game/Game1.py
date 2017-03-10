6efd83c1338f87f43ad0a85546333125949b416f
Hoang Tien Nhat Anh <nhatanh120795@gmail.com>
2017-03-10 10:28:05 +0700

    Homework Day 8

--- Game/Game1.py
+++ Game/Game1.py
@@ -4,19 +4,26 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 
 done = False
+game_finish = False
+game_lose = False
 
 # Draw background
-COLOR_WHILE = (255,255,255)
+COLOR_WHITE = (200, 255, 255)
 roger = pygame.image.load("mario.png")
 square_image = pygame.image.load("square.png")
+box_image = pygame.image.load("box.png")
+gate_image = pygame.image.load("gate.png")
+win_image = pygame.image.load("win.png")
+lose_image = pygame.image.load("lose.png")
 
 x = 100
 y = 100
 
 SQUARE_SIZE = 32
 
+
 class Player:
-    def __init__(self,x, y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
 
@@ -27,30 +34,70 @@ class Player:
     def cal_next_position(self, dx, dy):
         return [self.x + dx, self.y + dy]
 
+
+class Box:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+    def cal_next_position(self, dx, dy):
+        return [self.x + dx, self.y + dy]
+
+
+class Gate:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+
 class Map:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.player = Player(1,1)
+        self.player = Player(1, 1)
+        self.box = Box(6,6)
+        self.gate = Gate(7, 7)
 
     def move_player(self, dx, dy):
         [next_player_x, next_player_y] = self.player.cal_next_position(dx, dy)
+        [next_box_x, next_box_y] = self.box.cal_next_position(dx, dy)
         if self.check_inside(next_player_x, next_player_y):
-            self.player.move(dx, dy)
+            if next_player_x == self.box.x and next_player_y == self.box.y:
+                if self.check_inside(next_box_y, next_box_y):
+                    self.box.move(dx, dy)
+                    self.player.move(dx, dy)
+            else:
+                self.player.move(dx, dy)
 
     def check_inside(self, x, y):
-        return 0 <= x < self.width and 0<= y < self.height
+        return 0 <= x < self.width and 0 <= y < self.height
 
-map = Map(5,5)
+    def check_win(self):
+        if self.box.x == self.gate.x and self.box.y == self.gate.y:
+            return True
+        return False
+
+    def check_lose(self):
+        if [self.box.x, self.box.y] == [0, 0] or [self.box.x, self.box.y] == [self.width - 1, 0] or [self.box.x,
+                                                                                                     self.box.y] == [0,
+                                                                                                                     self.height - 1]:
+            return True
+        return False
+
+
+map = Map(10, 10)
 # Draw images
 
 while not done:
 
     key_arrow = None
 
-
     # Get events
-    dx, dy = 0 , 0
+    dx, dy = 0, 0
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
@@ -67,17 +114,29 @@ while not done:
                 # x -= SQUARE_SIZE
 
 
-     # Process game events
-    if dx != 0  or dy != 0:
+                # Process game events
+    if dx != 0 or dy != 0:
         map.move_player(dx, dy)
-     # Repaint!
-    screen.fill(COLOR_WHILE)
+        if map.check_win():
+            print("you win!")
+            game_finish = True
+        if map.check_lose():
+            print("you lose")
+            game_lose = True
+            # Repaint!
+    screen.fill(COLOR_WHITE)
 
-    for y in range (map.height):
+    for y in range(map.height):
         for x in range(map.width):
-            screen.blit(square_image,(x * SQUARE_SIZE, y * SQUARE_SIZE) )
+            screen.blit(square_image, (x * SQUARE_SIZE, y * SQUARE_SIZE))
 
     screen.blit(roger, (map.player.x * SQUARE_SIZE, map.player.y * SQUARE_SIZE))
+    screen.blit(box_image, (map.box.x * SQUARE_SIZE, map.box.y * SQUARE_SIZE))
+    screen.blit(gate_image, (map.gate.x + SQUARE_SIZE, map.gate.y * SQUARE_SIZE))
 
+    if game_finish == True:
+        screen.blit(win_image, (100, 100))
 
+    if game_lose == True:
+        screen.blit(lose_image, (100, 100))
     pygame.display.flip()
--- Game/map.py
+++ Game/map.py
@@ -1,7 +1,32 @@
+import pygame
+import os
+
+_image_library = {}
 
 
-[next_x, next_y] = self.player.calc_next(dx, dy)
-    if self.check
+def get_image(path):
+    global _image_library
+    image = _image_library.get(path)
+    if image == None:
+        canonicalized_path = path.replace('/', os.sep).replace('\\', os.sep)
+        image = pygame.image.load(canonicalized_path)
+        _image_library[path] = image
+    return image
 
 
-def process_movement(self):
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+done = False
+clock = pygame.time.Clock()
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    screen.fill((255, 255, 255))
+
+    screen.blit(get_image('mario.png'), (20, 20))
+
+    pygame.display.flip()
+    clock.tick(60)
