da586ad319c080931d0dfc68812cd08692aa8e00
Hoang Tien Nhat Anh <nhatanh120795@gmail.com>
2017-03-25 08:55:15 +0700

    Session 13 on class

--- Tef/Tef.py
+++ Tef/Tef.py
@@ -1,19 +1,57 @@
 from flask import *
+import mlab
+from models.CarItems import CarItems
+
 
 app = Flask(__name__)
 
 
+#connect to mlab database
+mlab.connect()
+
+#create a new CarItems and save it to database
+# new_car = CarItems()
+# new_car.src = "http://st.motortrend.com/uploads/sites/5/2012/12/2014-BMW-M6-Gran-Coupe-rear-three-quarters.jpg"
+# new_car.title = "Item 2"
+# new_car.description = "Description for Item 2"
+# new_car.save()
+
+
+
 @app.route('/')
 def hello_world():
-    return render_template("commingsoon.html")
+    return redirect(url_for("foodblog"))
 
-@app.route('/w3cssdemo')
-def w3cssdemo():
-    return render_template("w3cssdemo.html")
+
+@app.route('/addcars', methods=["GET", "POST"])
+def add_cars():
+    if request.method == "GET":
+        return render_template("addcars.html")
+    if request.method == "POST":
+        new_car = CarItems()
+        new_car.src = request.form["source"]
+        new_car.title = request.form["title"]
+        new_car.image = request.f("image")
+        new_car.description = request.form["description"]
+        new_car.save()
+        return render_template("addcars.html")
+
+
+@app.route('/deletecars', methods=["GET", "POST"])
+def delete_cars():
+    if request.method == "GET":
+        return render_template("deletecars.html")
+    if request.method == "POST":
+        new_car = CarItems.objects(title=request.form["title"]).first()
+        if new_car is not None:
+            new_car.delete()
+        return render_template("deletecars.html")
+
+@app.route('/updatefood')
 
 @app.route('/foodblog')
 def foodblog():
-    return render_template("foodblog.html")
+    return render_template("foodblog.html", car_items=CarItems.objects())
 
 if __name__ == '__main__':
     app.run()
